bff5bc5a8d2e6b99d39929abc01450355d2f140a
Marco <markolas14@gmail.com>
2020-11-29 11:45:42 +0100

    Revert ".sql para la creacion de tablas"
    This reverts commit e8c943325f45045ed1ba87c0985ca86cc1fce408.
    
    .sql queda en el repositorio LDST2

--- sql/tabla_per_asig.sql
+++
@@ -1,4 +0,0 @@
-create table per_asig(
-email char(100) not null,
-codigo int unsigned not null,
-primary key (email,codigo));
--- sql/tabla_test.sql
+++
@@ -1,8 +0,0 @@
-create table test(
-id int unsigned not null auto_increment,
-email char(100) not null,
-f_test1 date,
-f_test2 date,
-f_descon date,
-comentario text(10000),
-primary key (id,email)); 
